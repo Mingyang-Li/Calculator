66a06d99604a93b5661c364dea72f925deba1032
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2019-11-25 09:49:46 +1300

    Writing my own JavaScript
    Wrote my own JavaScript file without looking at any online resources except MDN documentation, took my 1 hour.

--- script.js
+++ script.js
@@ -1,101 +1,75 @@
-const operators = document.querySelectorAll(".operators div");
-const numbers = document.querySelectorAll(".threetimesthree div,#zero");
+// define variables for buttons
+
+const clickedNumAndOps = document.querySelectorAll(".threetimesthree,.zero，.operators");
+const calculate = document.getElementById("equal");
 const clear = document.getElementById("clear");
-const result = document.getElementById("equal");
 const input = document.getElementById("input");
+const inputString = input.innerHTML;
 const resultDisplayed = false;
 
-//add click handler to the number buttons
-for (var i=0; i < numbers.length; i++){
-    numbers[i].addEventListener("click", function(e){
-        var currentString = input.innerHTML;
-        var lastChar = currentString[currentString.length-1];
-
-        if(resultDisplayed == false){
-            input.innerHTML += e.target.innerHTML;
-        }
-        else if(resultDisplayed == true && lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/"){
-            resultDisplayed == false;
-            input.innerHTML += e.target.innerHTML;
-        }
-        else{
-            resultDisplayed == false;
-            input.innerHTML = "";
-            input.innerHTML += e.target.innerHTML;
-        }
-    });
+//get user input before they click "=" sign
+for (var i = 0; i < clickedNumAndOps.length; i++){
+    if (clickedNumAndOps[i].clicked == true){
+        inputString += clickedNumAndOps[i];
+    }
 }
 
-//add click handler to operator buttons
-for (var i=0; 1<operators.length; i++){
-    operators[i].addEventListener("click", function(e){
-        var currentString = operators[i].innerHTML;
-        var lastChar = currentString[currentString.length - 1];
-        if (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/"){
-            var newString = currentString.substring(0, currentString.length-1) + e.target.innerHTML;
-            input.innerHTML = newString;
+//split input string by operators so we have an array of numbers 
+const numbers = inputString.split(/ \+ | \- | \* | \/ /);
+
+//split inpit string by numbers so we have an array of operators 
+const operators = inputString.replace(/[0-9]|\./g, "");
+
+//function to calculate
+function toCalculate(inputString, numbers, operators, resultDisplayed){
+    while (resultDisplayed == false){
+        while ("/" in inputString && inputString.indexOf("/" != -1)){
+            divideIndex = operators.indexOf("/");
+            first = numbers[divideIndex];
+            second = numbers[divideIndex+1];
+            newNum = first / second;
+            numbers.splice(numbers.indexOf(first), 2, newNum);
+        } 
+        inputString -= "/"
+    
+        while ("*" in inputString && inputString.indexOf("*" != -1)){
+            timesIndex = operators.indexOf("*");
+            first = numbers[timesIndex];
+            second = numbers[timesIndex+1];
+            newNum = first * second;
+            numbers.splice(numbers.indexOf(first), 2, newNum);
         }
-        else if (currentString.length == 0){
-            console.log("Enter a number first");
+        inputString -= "*"
+    
+        while ("+" in inputString && inputString.indexOf("+" != -1)){
+            plusIndex = operators.indexOf("+");
+            first = numbers[plusIndex];
+            second = numbers[plusIndex+1];
+            newNum = parseFloat(first) + parseFloat(second);
+            numbers.splice(numbers.indexOf(first), 2, newNum);
+        }inputString -= "+"
+    
+        while ("-" in inputString && inputString.indexOf("-" != -1)){
+            minusIndex = operators.indexOf("-");
+            first = numbers[minusIndex];
+            second = numbers[minusIndex+1];
+            newNum = parseFloat(first) - parseFloat(second);
+            numbers.splice(numbers.indexOf(first), 2, newNum);
         }
-        else{
-            input.innerHTML += e.target.innerHTML;
-        }
-    });
+        inputString -= "-"
+    }
+    resultDisdplayed = !resultDisdplayed;
 }
 
-//click handler for equal (calculate button)
-result.addEventListener("click", function(){
-    var inputString = input.innerHTML;
-    //splitting string by operators, so we have a list of numbers
-    var numbers = inputString.split(/\+|\-|\×|\÷/g);
-    //split the string by numbers so we have a list of operators
-    var operators = inputString.replace(/[0-9]|\./g, "").split("");
-    console.log(inputString);
-    console.log(numbers);
-    console.log(operators);
+//function to clear
+function toClear(){
+    input.innerHTML = "";
+}
 
-    var divideIndex = operators.indexOf("/");
-    while (divideIndex != -1){
-        numbers.splice(divideIndex, 2, numbers[divideIndex]/numbers[divideIndex+1]);
-        operators.splice(divideIndex, 1);
-        divideIndex = operators.indexOf("/");
-    }
+//event handler for equal/calculate button
+calculate.addEventListener("click", calculate());
 
-    var timesIndex = operators.indexOf("*");
-    while (timesIndex != -1){
-        numbers.splice(timesIndex, 2, numbers[timesIndex]*numbers[timesIndex+1]);
-        operators.splice(timesIndex, 1);
-        timesIndex = operators.indexOf("*");
-    }
-
-    var minusIndex = operators.indexOf("-");
-    while (timesIndex != -1){
-        numbers.splice(minusIndex, 2, numbers[minusIndex]-numbers[minusIndex+1]);
-        operators.splice(minusIndex, 1);
-        minusIndex = operators.indexOf("-");
-    }
-    var plusIndex = operators.indexOf("+");
-    while (timesIndex != -1){
-        numbers.splice(plusIndex, 2, parseFloat(numbers[plusIndex])+parseFloat(numbers[plusIndex+1]));
-        operators.splice(plusIndex, 1);
-        plusIndex = operators.indexOf("+");
-    }
-
-    //displaying output
-    input.innerHTML = numbers[0];
-
-    //turing result displayed to true once it's displayed
-    resultDisplayed == true;
-});
-
-//clearing input when "CLEAR" is pressed
-
-clear.addEventListener("click", function(){
-    input.innerHTML = '';
-});
-
-
-
+//event handler to clear button
+clear.addEventListener("click", toClear());
 
 
